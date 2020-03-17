a11287dd17bebea28bf48cef67176c6c9808d3a0
rspai <rasitapai@gmail.com>
2020-03-17 17:02:03 -0400

    adding comments, removing debug stmts

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+"""
+@author: Rasita Pai
+Q: to check whether a one-to-one character mapping exists from one string, s1, to another string, s2.
+"""
+
+
 import sys
+
 
 def IsMapped(str1, str2):
     if len(str1) == len(str2):
