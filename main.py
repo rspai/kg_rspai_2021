f23f8153a0fd0af9b70968df4abf78233e822ce5
rspai <rasitapai@gmail.com>
2020-03-17 16:52:03 -0400

    adding main code

---
+++ main.py
@@ -0,0 +1,16 @@
+import sys
+
+def IsMapped(str1, str2):
+    if len(str1) == len(str2):
+        for char in str1:
+            if str1.count(char) > 1:
+                return 0
+        return 1
+    else:
+        return 0    
+        
+s1 = sys.argv[1]
+s2 = sys.argv[2]        
+# print('str1: ', s1)
+# print('str2: ', s2)
+print(bool(IsMapped(s1, s2)))
